41ef57880603ddf3d523b36d388be63bf52f30f1
Argann Bay-Eymère <argann.bay-eymere@epitech.eu>
2018-04-05 15:04:17 +0200

    * Fixed AI thanks to a wonderful man

--- src/ai.c
+++ src/ai.c
@@ -13,23 +13,23 @@
 void process_selection(int *map, int line_nb, int max_taken, \
 vector2i_t *vec)
 {
-	int moves_left = count_move_left(map, line_nb);
 	int index_maxline = get_index_maxline(map, line_nb);
+	int moves_left = count_move_left(map, line_nb);
 	int nim_sum = sum_nim(map, line_nb);
 
 	*vec = (vector2i_t){index_maxline, 1};
 	if (is_end_game(map, line_nb)) {
 		if (!(map[index_maxline] == 1 && (moves_left % 2) == 1))
-			vec->y = (map[index_maxline] - (moves_left % 2)) \
-% max_taken;
+			vec->y = ((map[index_maxline] - (moves_left % 2)) \
+% (max_taken + 1));
 		return;
 	}
 	if (nim_sum == 0)
 		return;
 	for (int i = 0; i < line_nb; i++)
 		if ((map[i] ^ nim_sum) < map[i]) {
-			*vec = (vector2i_t){i, (map[i] - (map[i] ^ nim_sum)) % \
-max_taken};
+			*vec = (vector2i_t){i, ((map[i] - (map[i] ^ nim_sum)) %\
+(max_taken + 1))};
 			return;
 		}
 }
